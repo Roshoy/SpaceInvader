aa6fc22205da734b5bbdb72f5b72b9e8d47e661c
Roshoy <amaciej@gmail.com>
2019-04-10 23:47:16 +0200

    Multiplayer for 2 add

--- Main/main.py
+++ Main/main.py
@@ -23,7 +23,7 @@ timer = 0
 
 hits = 0
 
-missile_wraper = MissileWrapper()
+missile_wrapper = MissileWrapper()
 
 while True:
     d_time = pygame.time.Clock().tick(60)
@@ -36,46 +36,33 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
-        # if event.type == pygame.KEYDOWN and event.key == player1.controls[4]:
-        #     missile_wraper.add_from_player(player1.shoot())
-        # if event.type == pygame.KEYDOWN and event.key == player1.controls[4]:
-        #     missile_wraper.add_from_player(player2.shoot())
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            pass
-            #missile_wraper.add_from_player(player.shoot())
-        if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0] == 1:
-            #missile_wraper.add_from_player(player.shoot())
-            pass
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit(0)
 
     screen.fill((0, 0, 0))
-    #player.update2(pygame.mouse.get_pos(), d_time, screen)
-    missile_wraper.add_from_player(player1.shoot())
-    missile_wraper.add_from_player(player2.shoot())
+    missile_wrapper.add_from_player(player1.shoot())
+    missile_wrapper.add_from_player(player2.shoot())
     player1.update22(screen, d_time)
     player2.update22(screen, d_time)
     for s in stars:
         s.update(screen)
         s.draw(screen)
 
-    missile_wraper.update(screen)
+    missile_wrapper.update(screen)
 
     for e in enemies:
         dp1 = Vector(player1.center[0] - e.center[0], player1.center[1] - e.center[1])
         dp2 = Vector(player2.center[0] - e.center[0], player2.center[1] - e.center[1])
         if dp1.magnitude() > dp2.magnitude():
-            missile_wraper.add_from_enemy(e.update(player2.center, d_time, screen))
+            missile_wrapper.add_from_enemy(e.update(player2.center, d_time, screen))
         else:
-            missile_wraper.add_from_enemy(e.update(player1.center, d_time, screen))
+            missile_wrapper.add_from_enemy(e.update(player1.center, d_time, screen))
         e.draw(screen)
 
-
-    enemies = [e for e in enemies if not missile_wraper.enemy_hit(e)]
-    missile_wraper.player_hit(player1)
-    missile_wraper.player_hit(player2)
-    missile_wraper.draw(screen)
+    enemies = [e for e in enemies if not missile_wrapper.enemy_hit(e)]
+    missile_wrapper.player_hit(player1)
+    missile_wrapper.player_hit(player2)
+    missile_wrapper.draw(screen)
     player1.draw(screen)
     player2.draw(screen)
-    #pygame.draw.rect(screen, (0, 255, 0), player)
     pygame.display.flip()
