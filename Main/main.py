131cc5166e978b5ecb5a31039f651adb1c603db4
kbieniasz <https://github.com/kbieniasz/ErlangBeginnings>
2019-05-07 23:04:59 +0200

    Test commit

--- Main/main.py
+++ Main/main.py
@@ -3,6 +3,7 @@ from Main.Engine import Engine
 from Main.menu import Menu
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
+# comment
 
 menu = Menu(screen, "Space Invaders Deluxe")
 menu.run()
