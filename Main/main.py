dcd58157da59b844016cbdc56f26eb939f21c4b6
kbieniasz <https://github.com/kbieniasz/ErlangBeginnings>
2019-05-18 20:11:09 +0200

    init sounds

--- Main/main.py
+++ Main/main.py
@@ -1,50 +1,7 @@
 import pygame
 from Main.menu import Menu
 
-# class A:
-#     heh = dict()
-#     a = 0
-#     @classmethod
-#     def m(cls):
-#         cls.heh = dict()
-#         cls.inc(4)
-#         print(str(cls.heh))
-#
-#     @classmethod
-#     def inc(cls, k):
-#         cls.a += k
-#         cls.heh.update({k: 1})
-#
-# class B(A):
-#     @classmethod
-#     def m(cls):
-#         cls.inc(1)
-#         print(cls.a)
-#         print(str(cls.heh))
-#
-# class C(A):
-#     @classmethod
-#     def m(cls):
-#         cls.heh = dict()
-#         #cls.heh.clear()
-#         print("B.heh = " + str(B.heh))
-#         cls.inc(2)
-#         print(cls.a)
-#         print(str(cls.heh))
-#
-# # class C(A, B):
-# #     def __init__(self):
-# #         A.__init__(self)
-# #         B.__init__(self)
-# #         print("C")
-#
-# B.m()
-# C.m()
-# A.m()
-# C.m()
-# print(str(A.heh))
-# B.m()
-#
+#test coment
 
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
